a1dc3d04312c22ca8ce31db1610f7b70abd32741
TTvanWillegen <mail@tobyvanwillegen.nl>
2019-12-04 13:34:19 +0100

    Init repo

---
+++ template/main.py
@@ -0,0 +1,18 @@
+def main():
+    part_one()
+    part_two()
+
+
+def part_one():
+    print("Part One")
+    with open("input.txt", "r") as input_file:
+        for line in input_file:
+            print(line)
+
+
+def part_two():
+    print("Part Two")
+
+
+if __name__ == "__main__":
+    main()
